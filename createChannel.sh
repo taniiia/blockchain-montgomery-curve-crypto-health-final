4421a3ff640485a1905d304cda0e339026eb5e05
taniiia <somannatania@gmail.com>
2025-03-26 05:33:35 +0530

    createChannel.sh file modified

--- createChannel.sh
+++ createChannel.sh
@@ -1,93 +1,100 @@
 export CORE_PEER_TLS_ENABLED=true
-export ORDERER_CA=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-export PEER0_ORG1_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export PEER0_ORG2_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+export ORDERER_CA=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/pesuhospital.com/orderers/orderer.pesuhospital.com/msp/tlscacerts/tlsca.pesuhospital.com-cert.pem
+export PEER0_PESUHOSPITALBLR_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/blr.pesuhospital.com/peers/peer0.blr.pesuhospital.com/tls/ca.crt
+export PEER0_PESUHOSPITALKPM_CA=${PWD}/artifacts/channel/crypto-config/peerOrganizations/kpm.pesuhospital.com/peers/peer0.kpm.pesuhospital.com/tls/ca.crt
 export FABRIC_CFG_PATH=${PWD}/artifacts/channel/config/
 
-export CHANNEL_NAME=mychannel
+export CHANNEL_NAME=RegistrationChannel
 
-# setGlobalsForOrderer(){
-#     export CORE_PEER_LOCALMSPID="OrdererMSP"
-#     export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-#     export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/example.com/users/Admin@example.com/msp
+setGlobalsForOrderer(){
+    export CORE_PEER_LOCALMSPID="OrdererMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/pesuhospital.com/orderers/orderer.pesuhospital.com/msp/tlscacerts/tlsca.pesuhospital.com-cert.pem
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/ordererOrganizations/pesuhospital.com/users/Admin@pesuhospital.com/msp
     
-# }
+}
 
-setGlobalsForPeer0Org1(){
-    export CORE_PEER_LOCALMSPID="Org1MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+setGlobalsForPeer0BLR(){
+    export CORE_PEER_LOCALMSPID="PESUHospitalBLRMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_PESUHOSPITALBLR_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/blr.pesuhospital.com/users/Admin@blr.pesuhospital.com/msp
     export CORE_PEER_ADDRESS=localhost:7051
 }
 
-setGlobalsForPeer1Org1(){
-    export CORE_PEER_LOCALMSPID="Org1MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+setGlobalsForPeer1BLR(){
+    export CORE_PEER_LOCALMSPID="PESUHospitalBLRMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_PESUHOSPITALBLR_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/blr.pesuhospital.com/users/Admin@blr.pesuhospital.com/msp
     export CORE_PEER_ADDRESS=localhost:8051
     
 }
 
-setGlobalsForPeer0Org2(){
-    export CORE_PEER_LOCALMSPID="Org2MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+setGlobalsForPeer0KPM(){
+    export CORE_PEER_LOCALMSPID="PESUHospitalKPMMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_PESUHOSPITALKPM_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/kpm.pesuhospital.com/users/Admin@kpm.pesuhospital.com/msp
     export CORE_PEER_ADDRESS=localhost:9051
     
 }
 
-setGlobalsForPeer1Org2(){
-    export CORE_PEER_LOCALMSPID="Org2MSP"
-    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
-    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+setGlobalsForPeer1KPM(){
+    export CORE_PEER_LOCALMSPID="PESUHospitalKPMMSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_PESUHOSPITALKPM_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/kpm.pesuhospital.com/users/Admin@kpm.pesuhospital.com/msp
     export CORE_PEER_ADDRESS=localhost:10051
     
 }
 
 createChannel(){
+    echo "Removing old channel artifacts..."
     rm -rf ./channel-artifacts/*
-    setGlobalsForPeer0Org1
+
+    echo "Creating channel '$CHANNEL_NAME'..."
+    setGlobalsForPeer0BLR
     
     peer channel create -o localhost:7050 -c $CHANNEL_NAME \
-    --ordererTLSHostnameOverride orderer.example.com \
+    --ordererTLSHostnameOverride orderer.pesuhospital.com \
     -f ./artifacts/channel/${CHANNEL_NAME}.tx --outputBlock ./channel-artifacts/${CHANNEL_NAME}.block \
     --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
 }
 
 removeOldCrypto(){
+    echo "Removing old crypto material from API wallets..."
     rm -rf ./api-1.4/crypto/*
-    rm -rf ./api-1.4/fabric-client-kv-org1/*
-    rm -rf ./api-2.0/org1-wallet/*
-    rm -rf ./api-2.0/org2-wallet/*
+    rm -rf ./api-1.4/fabric-client-kv-blr/*
+    rm -rf ./api-2.0/blr-wallet/*
+    rm -rf ./api-2.0/kpm-wallet/*
 }
 
 
 joinChannel(){
-    setGlobalsForPeer0Org1
+    echo "Joining Peers to channel '$CHANNEL_NAME'..."
+
+    setGlobalsForPeer0BLR
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
-    setGlobalsForPeer1Org1
+    setGlobalsForPeer1BLR
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
-    setGlobalsForPeer0Org2
+    setGlobalsForPeer0KPM
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
-    setGlobalsForPeer1Org2
+    setGlobalsForPeer1KPM
     peer channel join -b ./channel-artifacts/$CHANNEL_NAME.block
     
 }
 
 updateAnchorPeers(){
-    setGlobalsForPeer0Org1
-    peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
+    echo "Updating anchor peers for PESUHospitalBLR..."
+    setGlobalsForPeer0BLR
+    peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.pesuhospital.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
     
-    setGlobalsForPeer0Org2
-    peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA
-    
+    echo "Updating anchor peers for PESUHospitalKPM..."
+    setGlobalsForPeer0KPM
+    peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.pesuhospital.com -c $CHANNEL_NAME -f ./artifacts/channel/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA    
 }
 
-removeOldCrypto
+# removeOldCrypto
 
 createChannel
-joinChannel
-updateAnchorPeers
+# joinChannel
+# updateAnchorPeers
